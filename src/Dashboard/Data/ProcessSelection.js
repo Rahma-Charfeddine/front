605002640396c5577f4216ba98d316e57ff3cdef
hajalimazen__ <hadjalimazen@gmail.com>
2023-06-11 19:53:16 +0100

    1953

--- src/Dashboard/Data/ProcessSelection.js
+++ src/Dashboard/Data/ProcessSelection.js
@@ -11,6 +11,7 @@ function ProcessSelection() {
     const [addingSection, setAddingSection] = useState(false);
 
     const [name, setname] = useState("");
+    const [message, setMessage] = useState({ code: 0, message: "" });
 
 
     useEffect(() => {
@@ -30,16 +31,39 @@ function ProcessSelection() {
         instance.post("process", {
             process_name: name
         })
+            .then(response => {
+                setMessage({ code: 200, message: "Process : " + response.data.process_name + " Added successfully." });
+                setProcesses(prev => ([...prev, response.data]));
+            })
+            .catch(err => {
+                console.log(err)
+                setMessage({ code: 500, message: "Something went Wrong while creating new Process." })
+            })
     }
 
     return (
         <React.Fragment>
             {(addingSection && (
+
                 <div className="container mt-5">
+                    {message.code === 200 && (
+                        <div className="container">
+                            <div className="alert alert-success" role="alert">
+                                {message.message}
+                            </div>
+                        </div>
+                    )}
+                    {message.code === 500 && (
+                        <div className="container">
+                            <div className="alert alert-danger" role="alert">
+                                {message.message}
+                            </div>
+                        </div>
+                    )}
                     <form onSubmit={onSubmitNewProcess}>
                         <span className="text-primary"><strong>New Process :</strong></span>
                         <div class="input-group">
-                            <input required type="text" class="form-control" placeholder="Process Name..." aria-label="Process Name..." onChange={(e) => { setname(e.target.value) }} />
+                            <input required type="text" class="form-control" placeholder="Process Name..." aria-label="Process Name..." value={name} onChange={(e) => { setname(e.target.value) }} />
                             <button class="btn btn-outline-secondary" type="reset">Reset</button>
                             <button class="btn btn-outline-primary" type="submit">Submit</button>
                         </div>
@@ -51,20 +75,18 @@ function ProcessSelection() {
                 </div>
 
             }
-            <div className="conatiner p-5 m-5 d-flex justify-content-center align-items-center">
-                <div class="d-grid gap-2">
-                    {
-                        processes.map((process, index) => (
-                            <button
-                                class="btn btn-primary"
-                                type="button" onClick={() => {
-                                    navigate('/dashboard/data/' + process._id)
-                                }}>
-                                {process.process_name}
-                            </button>
-                        ))
-                    }
-                </div>
+            <div className="conatiner px-5 m-5 text-center">
+                {
+                    processes.map((process, index) => (
+                        <button
+                            class="btn btn-lg btn-outline-primary w-100 m-3 text-capitalize fw-bolder px-5"
+                            type="button" onClick={() => {
+                                navigate('/dashboard/data/' + process._id)
+                            }}>
+                            {process.process_name}
+                        </button>
+                    ))
+                }
             </div>
         </React.Fragment>
     )
