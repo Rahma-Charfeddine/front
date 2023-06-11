fd4e64c5ea843a5d64f2e0697f39240c5caed720
hajalimazen__ <hadjalimazen@gmail.com>
2023-06-12 00:05:50 +0100

    0005

--- src/Dashboard/OverView/index.js
+++ src/Dashboard/OverView/index.js
@@ -1,6 +1,67 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import Chart from 'chart.js/auto';
+import { getRelativePosition } from 'chart.js/helpers';
+import { Bar } from "react-chartjs-2";
+
 
 function OverView() {
+
+    const canvas = useRef();
+
+    const Data = [
+        {
+            id: 1,
+            year: 2016,
+            userGain: 80000,
+            userLost: 823
+        },
+        {
+            id: 2,
+            year: 2017,
+            userGain: 45677,
+            userLost: 345
+        },
+        {
+            id: 3,
+            year: 2018,
+            userGain: 78888,
+            userLost: 555
+        },
+        {
+            id: 4,
+            year: 2019,
+            userGain: 90000,
+            userLost: 4555
+        },
+        {
+            id: 5,
+            year: 2020,
+            userGain: 4300,
+            userLost: 234
+        }
+    ];
+
+    const [chartData, setChartData] = useState({
+        labels: Data.map((data) => data.year),
+        datasets: [
+            {
+                label: "Users Gained ",
+                data: Data.map((data) => data.userGain),
+                backgroundColor: [
+                    "rgba(75,192,192,1)",
+                    "& quot; #ecf0f1",
+                    "#50AF95",
+                    "#f3ba2f",
+                    "#2a71d0"
+                ],
+                borderColor: "black",
+                borderWidth: 2
+            }
+        ]
+    });
+
+
+
     return (
         <React.Fragment>
             <div className="container">
@@ -33,7 +94,24 @@ function OverView() {
                 <hr />
 
             </div>
-            <div className="container m-4">
+            <div className="container d-flex justify-content-center">
+                <div className="chart-container w-100">
+                    <h2 className="text-center">Bar Chart</h2>
+                    <Bar
+                        data={chartData}
+                        options={{
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: "Users Gained between 2016-2020"
+                                },
+                                legend: {
+                                    display: false
+                                }
+                            }
+                        }}
+                    />
+                </div>
             </div>
         </React.Fragment>
     )
