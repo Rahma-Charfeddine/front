459ba6115894f3659531c5c936606041cf39a586
hajalimazen__ <hadjalimazen@gmail.com>
2023-06-12 10:19:19 +0100

    1019

--- src/Dashboard/Data/FinalStep.js
+++ src/Dashboard/Data/FinalStep.js
@@ -13,6 +13,18 @@ function FinalStep() {
     const [elements, setElement] = useState([]);
     const [indicators, setIndicators] = useState([]);
 
+    const [addingSection, setAddingSection] = useState(false);
+    const [message, setMessage] = useState({ code: 0, message: "" });
+    const [newIndicator, setNewIndicator] = useState({
+        element_id: idelement,
+        indicator_name: "",
+        unit: "",
+        goal: 0,
+        achieved: 0,
+        is_max: "true"
+    });
+
+
     useEffect(() => {
         instance.get('process').then(response => {
             setProcesses(response.data.data.map(el => ({ value: el._id, label: el.process_name })))
@@ -32,12 +44,27 @@ function FinalStep() {
 
     }, [idprocess, idsub, idelement])
 
+    const onSubmitNewIndicator = async (e) => {
+        e.preventDefault();
+        await instance.post('indicator', newIndicator)
+            .then(response => {
+                setMessage({ code: 200, message: "Indicator : " + response.data.indicator_name + " Added successfully." });
+                setIndicators(prev => ([...prev, response.data]));
+            })
+            .catch(err => {
+                console.log(err)
+                setMessage({ code: 500, message: "Something went Wrong while creating new Indicator." })
+            })
+    }
+
+
+
     const checked = () => {
         return (
             <React.Fragment>
                 <td className="text-center"><div className="p-2 bg-success" style={{ width: '5px', height: '10px' }}></div></td>
-                <td className="text-center"><i class="bi bi-check-circle"></i></td>
-                <td className="text-center"><i class="bi bi-check-circle"></i></td>
+                <td className="text-center"><i className="bi bi-check-circle"></i></td>
+                <td className="text-center"><i className="bi bi-check-circle"></i></td>
             </React.Fragment>
         )
     }
@@ -45,12 +72,10 @@ function FinalStep() {
     const notchecked = () => {
         return (<React.Fragment>
             <td className="text-center">Red</td>
-            <td className="text-center"><i class="bi bi-circle"></i></td>
-            <td className="text-center"><i class="bi bi-circle"></i></td>
+            <td className="text-center"><i className="bi bi-circle"></i></td>
+            <td className="text-center"><i className="bi bi-circle"></i></td>
         </React.Fragment>)
     }
-
-
 
     return (
         <React.Fragment>
@@ -58,22 +83,22 @@ function FinalStep() {
             <div className="container">
                 <div className="row">
                     <div className="col-md-4 d-flex justify-content-center align-items-center">
-                        <i class="bi bi-terminal me-2"></i>
-                        <span>Processes :</span>
+                        <i className="bi bi-terminal me-2"></i>
+                        <span><strong>Processes :</strong></span>
                         <Select className="w-50 px-2" options={processes} value={
                             processes.filter(option =>
                                 option.value === idprocess)
                         } />
                     </div>
                     <div className="col-md-4 d-flex justify-content-center align-items-center">
-                        <span>Sub-Processes :</span>
+                        <span><strong>Sub-Processes :</strong></span>
                         <Select className="w-50 px-2" options={subprocesses} value={
                             subprocesses.filter(option =>
                                 option.value === idsub)
                         } />
                     </div>
                     <div className="col-md-4 d-flex justify-content-center align-items-center">
-                        <span>Elements :</span>
+                        <span><strong>Elements :</strong></span>
                         <Select className="w-50 px-2" options={elements} value={
                             elements.filter(option =>
                                 option.value === idelement)
@@ -82,7 +107,58 @@ function FinalStep() {
                 </div>
             </div>
             <hr />
-
+            {!addingSection && <div className="container d-flex justify-content-end align-items-center py-2 my-2">
+                <button className="btn btn-outline-primary" onClick={() => { setAddingSection(true) }}>New Indicator</button>
+            </div>}
+            {addingSection && <div className="container py-2 my-2">
+                {message.code === 200 && (
+                    <div className="container">
+                        <div className="alert alert-success" role="alert">
+                            {message.message}
+                        </div>
+                    </div>
+                )}
+                {message.code === 500 && (
+                    <div className="container">
+                        <div className="alert alert-danger" role="alert">
+                            {message.message}
+                        </div>
+                    </div>
+                )}
+                <form onSubmit={onSubmitNewIndicator} className="p-3 m-2 border rounded shadow w-100">
+                    <span className="text-primary m-3"><strong>New Element :</strong></span>
+                    <hr />
+                    <div className="mb-3">
+                        <label htmlFor="element_name" className="form-label">Indicator Name</label>
+                        <input type="text" className="form-control" id="element_name" value={newIndicator?.indicator_name} onChange={e => { setNewIndicator(prev => ({ ...prev, indicator_name: e.target.value })) }} />
+                        <div id="emailHelp" className="form-text">the Name of the Indicator that will be added</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="element_name" className="form-label">Unit</label>
+                        <input type="text" className="form-control" id="element_name" value={newIndicator?.unit} onChange={e => { setNewIndicator(prev => ({ ...prev, unit: e.target.value })) }} />
+                        <div id="emailHelp" className="form-text">the unit of the Element that will be mesured with!</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">Goal</label>
+                        <input type="number" className="form-control" id="description" value={newIndicator?.goal} onChange={e => { setNewIndicator(prev => ({ ...prev, goal: e.target.value })) }} />
+                        <div id="emailHelp" className="form-text">the main Goal/Limit</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">is_max</label>
+                        <select className="form-select" aria-label="Default select example" value={newIndicator?.is_max === "true"} onChange={e => { setNewIndicator(prev => ({ ...prev, is_max: e.target.value })) }} >
+                            <option value="true">Maximazation</option>
+                            <option value="false">Limitaion</option>
+                        </select>
+                        <div id="emailHelp" className="form-text">is it a maximazation or a limitaion !</div>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                        <button className="btn btn-outline-danger mx-2" type="reset" onClick={() => { setAddingSection(false) }}>Cancel</button>
+                        <button className="btn btn-outline-secondary mx-2" type="reset">Reset</button>
+                        <button className="btn btn-outline-primary mx-2" type="submit">Submit</button>
+                    </div>
+                    <hr />
+                </form>
+            </div>}
             <div className="container-fluid  border p-5 border">
                 <table className="table" align="center">
                     <thead>
@@ -105,7 +181,7 @@ function FinalStep() {
                                 <td>{indicator.goal}</td>
                                 <td>{indicator.achieved}</td>
                                 {!indicator.is_max ? ((indicator.achieved - indicator.goal) * (-1) >= 0 ? checked() : notchecked()) : ((indicator.achieved - indicator.goal) >= 0 ? checked() : notchecked())}
-                                <td></td>
+                                <td> -- </td>
                             </tr>
                         ))}
                     </tbody>
--- src/Login.js
+++ src/Login.js
@@ -40,35 +40,39 @@ function Login() {
             }
             {
                 !is_Loading && (
-                    <form onSubmit={submitlogin} className="text-center">
-                        <img class="mb-4" src="/logo.webp" style={{ filter: "invert()", maxWidth: '300px' }} alt="" />
-                        {
-                            err && (
-                                <div class="alert alert-danger" role="alert">
-                                    Please Verify your Credentials
+                    <div class="card">
+                        <div class="card-body p-5">
+                            <h5 class="card-title mb-3 text-center"><strong><u>Login Form</u></strong></h5>
+                            <form onSubmit={submitlogin} className="text-center">
+                                <img class="m-4" src="/logo.webp" style={{ filter: "invert()", maxWidth: '300px' }} alt="" />
+                                {
+                                    err && (
+                                        <div class="alert alert-danger" role="alert">
+                                            Please Verify your Credentials
+                                        </div>
+                                    )
+                                }
+                                <p>Please Enter Your Credentials :</p>
+                                <div className="form-floating m-2">
+                                    <input type="text" className="form-control" id="registartion_number"
+                                        defaultValue={registartion_number}
+                                        onChange={e => { setRegistartionNumber(e.target.value) }}
+                                    />
+                                    <label htmlFor="registartion_number">Registration number :</label>
+                                    <div className="invalid-feedback">Registration number is required.</div>
                                 </div>
-                            )
-                        }
-                        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-                        <div className="form-floating m-2">
-                            <input type="text" className="form-control" id="registartion_number"
-                                defaultValue={registartion_number}
-                                onChange={e => { setRegistartionNumber(e.target.value) }}
-                            />
-                            <label htmlFor="registartion_number">Registration number :</label>
-                            <div className="invalid-feedback">Registration number is required.</div>
+                                <div className="form-floating m-2">
+                                    <input type="password" className="form-control" id="password"
+                                        defaultValue={password}
+                                        onChange={e => { setPassword(e.target.value) }}
+                                    />
+                                    <label htmlFor="registartion_number">Password :</label>
+                                </div>
+                                <button class=" btn btn-lg btn-primary m-3" type="submit">Sign in</button>
+                                <p class="mt-5 mb-3 text-muted"> © CopyRight Reserved 2023</p>
+                            </form>
                         </div>
-                        <div className="form-floating m-2">
-                            <input type="password" className="form-control" id="password"
-                                defaultValue={password}
-                                onChange={e => { setPassword(e.target.value) }}
-                            />
-                            <label htmlFor="registartion_number">Password :</label>
-                        </div>
-                        <button class=" btn btn-lg btn-primary m-3" type="submit">Sign in</button>
-                        <p class="mt-5 mb-3 text-muted"> © 2023</p>
-                    </form>
+                    </div>
                 )
             }
         </div>
